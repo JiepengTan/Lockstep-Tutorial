89c9a1be987bdb36c66a20187dba90ab3e80b8df
jiepengtan <jiepengtan@gmail.com>
2019-08-20 23:57:28 +0800

    [+] update Readme

--- Unity/Assets/Scripts/Core/Base/PureBaseManager.cs
+++ Unity/Assets/Scripts/Core/Base/PureBaseManager.cs
@@ -2,7 +2,7 @@ using Lockstep.Math;
 using UnityEngine;
 
 namespace Lockstep.Logic {
-    public abstract class PureBaseManager : MonoBehaviour, IManager {
+    public abstract class PureBaseManager : IManager {
         public virtual void DoAwake(){ }
         public virtual void DoStart(){ }
         public virtual void DoUpdate(LFloat deltaTime){ }
