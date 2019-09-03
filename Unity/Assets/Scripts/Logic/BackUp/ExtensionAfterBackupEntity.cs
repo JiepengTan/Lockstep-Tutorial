740d2c5e7f09a817539290904e21cc19bf4507fd
jiepengtan <jiepengtan@gmail.com>
2019-09-03 13:23:05 +0800

    [*] 调整目录结构

--- Unity/Assets/Scripts/Logic/BackUp/ExtensionAfterBackupEntity.cs
+++ Unity/Assets/Scripts/Logic/BackUp/ExtensionAfterBackupEntity.cs
@@ -1,18 +1,8 @@
 using Lockstep.Game;
 
 namespace Lockstep.Game {
-    public partial class Enemy : IAfterBackup {
-        public void OnAfterDeserialize(){
-            
-        }
-    }
-
-    public partial class Player : IAfterBackup {
-        public void OnAfterDeserialize(){ }
-    }
-
-    public partial class Spawner : IAfterBackup {
-        public void OnAfterDeserialize(){ }
-    }
+    public partial class Enemy : IAfterBackup {public void OnAfterDeserialize(){}}
+    public partial class Player : IAfterBackup {public void OnAfterDeserialize(){ }}
+    public partial class Spawner : IAfterBackup {public void OnAfterDeserialize(){ }}
 
 }
